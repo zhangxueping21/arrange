d49904fdb63054a25b1d019c1d10bf653b261654
zhangxueping21 <zhangxueping21@foxmail.com>
2020-10-28 17:13:35 +0800

    初次提交

---
+++ src/main/java/com/arrange/ArrangeApplication.java
@@ -0,0 +1,13 @@
+package com.arrange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArrangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArrangeApplication.class, args);
+    }
+
+}
