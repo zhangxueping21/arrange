ce52bd9edda13ad0b0ddc8ce0613b24fb7ae2172
zhangxueping21 <zhangxueping21@foxmail.com>
2020-10-28 17:14:53 +0800

    初次提交

---
+++ src/test/java/com/arrange/ArrangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arrange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArrangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
